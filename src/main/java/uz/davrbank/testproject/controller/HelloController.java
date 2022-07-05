97231a84af066082347476d889a7d4e6bc767e42
rahmonali96 <rahmonaliyoqubov@gmail.com>
2022-07-05 16:03:19 +0500

    2022-07-05-3

--- src/main/java/uz/davrbank/testproject/controller/HelloController.java
+++ src/main/java/uz/davrbank/testproject/controller/HelloController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping
     public String get() {
-        return "CI/CD with Jenkins!!!";
+        return "CI/CD with Jenkins";
     }
 }
