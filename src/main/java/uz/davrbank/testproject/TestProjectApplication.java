dc16ba350f381a983aea7103c6167b016e0fc63c
rahmonali96 <rahmonaliyoqubov@gmail.com>
2022-07-05 15:44:02 +0500

    2022-07-05-1

---
+++ src/main/java/uz/davrbank/testproject/TestProjectApplication.java
@@ -0,0 +1,13 @@
+package uz.davrbank.testproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestProjectApplication.class, args);
+    }
+
+}
---
+++ src/main/java/uz/davrbank/testproject/controller/HelloController.java
@@ -0,0 +1,14 @@
+package uz.davrbank.testproject.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+public class HelloController {
+    @GetMapping
+    public String get() {
+        return "CI/CD with Jenkins";
+    }
+}
